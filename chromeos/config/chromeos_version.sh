3bd7c5f9747bb62436c9a2df349825c73bc81e5f
Scott Zawalski <scottz@google.com>
2011-03-10 01:37:31 -0800

    Bump top of tree to R12

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -18,7 +18,7 @@
 # Major/minor versions.
 # Primarily for product marketing.
 export CHROMEOS_VERSION_MAJOR=0
-export CHROMEOS_VERSION_MINOR=11
+export CHROMEOS_VERSION_MINOR=12
 
 # Branch number.
 # Increment by 1 in a new release branch.
