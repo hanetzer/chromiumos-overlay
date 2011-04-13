b5ab37a8f1e144b656633deddfe63c049ab1a38a
Zach Kuznia <zork@chromium.org>
2011-04-13 17:16:47 +0900

    Update the chromeos generation script to support remapping the right alt and ctrl keys.
    Change-Id: I51338c109d2dcc04162ec2523c2e8b5416c2fcac
    
    BUG=chromium-os:10091
    TEST=Remap the ctrl or alt key, and check that it affects the right side as well as the left.
    
    Review URL: http://codereview.chromium.org/6833017

--- x11-misc/xkeyboard-config/files/gen_symbols_chromeos.py
+++ x11-misc/xkeyboard-config/files/gen_symbols_chromeos.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -10,6 +10,7 @@ Control keys.)
 
 __author__ = "yusukes"
 
+import re
 import sys
 
 KEY_STRING_TO_HUMAN_READABLE_NAME = {
@@ -38,7 +39,7 @@ KEY_STRING_TO_MODIFIER_NAME = {
 
 
 def OutputRemapEntry(use_search_key_as, use_leftcontrol_key_as,
-                     use_leftalt_key_as):
+                     use_leftalt_key_as, keep_right_alt):
     """Outputs an XKB entry like this:
 
     // Search (Win-L) and CapsLock keys are mapped to Left Control.
@@ -49,10 +50,12 @@ def OutputRemapEntry(use_search_key_as, use_leftcontrol_key_as,
       key <LWIN> { [ Control_L ] };
       key <CAPS> { [ Control_L ] };
       key <LCTL> { [ Alt_L, Meta_L ] };
+      key <RCTL> { [ Alt_R, Meta_R ] };
       key <LALT> { [ Super_L ] };
-      modifier_map Mod4 { <LALT> };
+      key <RALT> { [ Super_R ] };
+      modifier_map Mod4 { <LALT>, <RALT> };
       modifier_map Control { <LWIN>, <CAPS> };
-      modifier_map Mod1 { <LCTL> };
+      modifier_map Mod1 { <LCTL>, <RCTL> };
     };
 
     Args:
@@ -69,31 +72,51 @@ def OutputRemapEntry(use_search_key_as, use_leftcontrol_key_as,
         'disabled': [],
     }
     modifier_keys[use_search_key_as] += ['<LWIN>', '<CAPS>']
-    modifier_keys[use_leftcontrol_key_as] += ['<LCTL>']
+    modifier_keys[use_leftcontrol_key_as] += ['<LCTL>', '<RCTL>']
     modifier_keys[use_leftalt_key_as] += ['<LALT>']
+    if not keep_right_alt:
+      modifier_keys[use_leftalt_key_as] += ['<RALT>']
 
     print ''
     print '// Search (Win-L) and CapsLock keys are mapped to %s.' % (
         KEY_STRING_TO_HUMAN_READABLE_NAME[use_search_key_as])
     print '// Left Control key is mapped to %s.' % (
         KEY_STRING_TO_HUMAN_READABLE_NAME[use_leftcontrol_key_as])
+    print '// Right Control key is mapped to %s.' % (
+        KEY_STRING_TO_HUMAN_READABLE_NAME[use_leftcontrol_key_as])
     print '// Left Alt key is mapped to %s.' % (
         KEY_STRING_TO_HUMAN_READABLE_NAME[use_leftalt_key_as])
+    if keep_right_alt:
+      print '// Right Alt key is not remapped'
+    else:
+      print '// Right Alt key is mapped to %s.' % (
+          KEY_STRING_TO_HUMAN_READABLE_NAME[use_leftalt_key_as])
     print 'partial modifier_keys'
-    print 'xkb_symbols "%s_%s_%s" {' % (
-        use_search_key_as, use_leftcontrol_key_as, use_leftalt_key_as)
+    if keep_right_alt:
+      print 'xkb_symbols "%s_%s_%s_keepralt" {' % (
+          use_search_key_as, use_leftcontrol_key_as, use_leftalt_key_as)
+    else:
+      print 'xkb_symbols "%s_%s_%s" {' % (
+          use_search_key_as, use_leftcontrol_key_as, use_leftalt_key_as)
     print '  key <LWIN> { [ %s ] };' % KEY_STRING_TO_KEYSYM[use_search_key_as]
     print '  key <CAPS> { [ %s ] };' % KEY_STRING_TO_KEYSYM[use_search_key_as]
     print '  key <LCTL> { [ %s ] };' % (
         KEY_STRING_TO_KEYSYM[use_leftcontrol_key_as])
+    print '  key <RCTL> { [ %s ] };' % (
+        re.sub(r'_L', r'_R', KEY_STRING_TO_KEYSYM[use_leftcontrol_key_as]))
 
     lalt = KEY_STRING_TO_KEYSYM[use_leftalt_key_as]
+    ralt = re.sub(r'_L', r'_R', KEY_STRING_TO_KEYSYM[use_leftalt_key_as])
     # We have to overwrite both level-1 (default: Alt_L) and level-2
     # (default: Meta_L) mappings.
     if lalt.find(',') == -1:
       print '  key <LALT> { [ %s, %s ] };' % (lalt, lalt)
+      if not keep_right_alt:
+        print '  key <RALT> { [ %s, %s ] };' % (ralt, ralt)
     else:
       print '  key <LALT> { [ %s ] };' % lalt
+      if not keep_right_alt:
+        print '  key <RALT> { [ %s ] };' % ralt
     for key in modifier_keys.keys():
         if key != 'disabled' and len(modifier_keys[key]) > 0:
             print '  modifier_map %s { %s };' % (
@@ -107,7 +130,9 @@ def main():
         for use_leftcontrol_key_as in key_strings:
             for use_leftalt_key_as in key_strings:
                 OutputRemapEntry(use_search_key_as, use_leftcontrol_key_as,
-                                 use_leftalt_key_as)
+                                 use_leftalt_key_as, False)
+                OutputRemapEntry(use_search_key_as, use_leftcontrol_key_as,
+                                 use_leftalt_key_as, True)
 
 if __name__ == '__main__':
   sys.exit(main())
