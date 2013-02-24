40dd6cef25d3749aff34a7beb426bb64ca8fa51d
Mandeep Singh Baines <msb@chromium.org>
2013-02-24 13:34:08 -0800

    chromeos-base: fix VT cursor re-enable on CROS_EMBEDDED
    busybox does not support setterm nor do we want to add it.
    
    BUG=chromium-os:38576
    TEST=Verified that I now get a cursos on VT2.
    
    Change-Id: I177e86fd09b18a47ab4536b79c2ee3392008f050
    Reviewed-on: https://gerrit.chromium.org/gerrit/43877
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mandeep Singh Baines <msb@chromium.org>
    Tested-by: Mandeep Singh Baines <msb@chromium.org>

--- chromeos-base/chromeos-base/files/cursor.sh
+++ chromeos-base/chromeos-base/files/cursor.sh
@@ -4,4 +4,7 @@
 # Re-enable it when launching a login shell.  This should only
 # happen when logging in via vt or crosh or ssh and those are
 # all fine.  Login shells shouldn't get launched normally.
-setterm -cursor on
+
+# Since busybox doesn't have setterm we'll use the ANSI escape sequence instead
+# http://en.wikipedia.org/wiki/ANSI_escape_code
+printf "\033[?25h\033[?0c" # setterm -cursor on
