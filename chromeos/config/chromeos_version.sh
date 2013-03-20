a8399c98ee6adb30c75b16a91ca11183df30c1b6
Chris Sosa <sosa@chromium.org>
2013-03-20 11:58:39 -0700

    Use the version specified in FLAGS_version if set.
    This lets callers like build_image actually override the version parts
    that might matter to it (like CHROMEOS_PATCH).
    
    BUG=chromium-os:222238
    TEST=Ran it with + without FLAG set.
    
    Change-Id: I9ed825100f546a0ba7a40439d5c8c7d33aa15655
    Reviewed-on: https://gerrit.chromium.org/gerrit/46020
    Commit-Queue: Chris Sosa <sosa@chromium.org>
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -12,35 +12,40 @@
 #############################################################################
 # SET VERSION NUMBERS
 #############################################################################
-# Release Build number.
-# Increment by 1 for every release build.
-export CHROMEOS_BUILD=3893
+if [ -z "${FLAGS_version}" ]; then
+  # Release Build number.
+  # Increment by 1 for every release build.
+  export CHROMEOS_BUILD=3893
 
-# Release Branch number.
-# Increment by 1 for every release build on a branch.
-# Reset to 0 when increasing release build number.
-export CHROMEOS_BRANCH=0
+  # Release Branch number.
+  # Increment by 1 for every release build on a branch.
+  # Reset to 0 when increasing release build number.
+  export CHROMEOS_BRANCH=0
 
-# Patch number.
-# Increment by 1 in case a non-scheduled branch release build is necessary.
-# Reset to 0 when increasing branch number.
-export CHROMEOS_PATCH=0
+  # Patch number.
+  # Increment by 1 in case a non-scheduled branch release build is necessary.
+  # Reset to 0 when increasing branch number.
+  export CHROMEOS_PATCH=0
+
+  # Official builds must set CHROMEOS_OFFICIAL=1.
+  if [ ${CHROMEOS_OFFICIAL:-0} -ne 1 ] && [ "${USER}" != "chrome-bot" ]; then
+    # For developer builds, overwrite CHROMEOS_VERSION_PATCH with a date string
+    # for use by auto-updater.
+    export CHROMEOS_PATCH=$(date +%Y_%m_%d_%H%M)
+  fi
+
+  # Version string. Not indentied to appease bash.
+  export CHROMEOS_VERSION_STRING=\
+"${CHROMEOS_BUILD}.${CHROMEOS_BRANCH}.${CHROMEOS_PATCH}"
+else
+  export CHROMEOS_BUILD=$(echo ${FLAGS_version} | cut -f 1 -d ".")
+  export CHROMEOS_BRANCH=$(echo ${FLAGS_version} | cut -f 2 -d ".")
+  export CHROMEOS_PATCH=$(echo ${FLAGS_version} | cut -f 3 -d ".")
+  export CHROMEOS_VERSION_STRING="${FLAGS_version}"
+fi
 
 # Major version for Chrome.
 export CHROME_BRANCH=27
-
-# Official builds must set CHROMEOS_OFFICIAL=1.
-if [ ${CHROMEOS_OFFICIAL:-0} -ne 1 ] && [ "${USER}" != "chrome-bot" ]; then
-  # For developer builds, overwrite CHROMEOS_VERSION_PATCH with a date string
-  # for use by auto-updater.
-  export CHROMEOS_PATCH=$(date +%Y_%m_%d_%H%M)
-fi
-
-# Version string. Not indentied to appease bash.
-export CHROMEOS_VERSION_STRING=\
-"${CHROMEOS_BUILD}.${CHROMEOS_BRANCH}"\
-".${CHROMEOS_PATCH}"
-
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
 export CHROME_BASE=
