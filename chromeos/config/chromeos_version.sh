c3cea06135cb7294ada325c49c25d9f741989086
Dharani Govindan <dharani@google.com>
2014-03-30 21:36:35 -0700

    Bump Chrome version after branching refs/heads/release-R35-5712.B
    Change-Id: I1fbd97e1f2ba0ca9dd2a4bbdc7f853e55fd0f536

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  export CHROMEOS_BUILD=5712
+  export CHROMEOS_BUILD=5713
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
@@ -45,7 +45,7 @@ else
 fi
 
 # Major version for Chrome.
-export CHROME_BRANCH=35
+export CHROME_BRANCH=36
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
 export CHROME_BASE=
