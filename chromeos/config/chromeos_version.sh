ee54bf875b0b2045d9eb763e3c9ecd08a1b2403d
Josafat Garcia <josafat@google.com>
2015-04-05 10:00:59 -0700

    Bump Chrome version after branching refs/heads/release-R43-6946.B
    Change-Id: I4e18756b753a4060cf8871d40f81e4f1aa0d8053

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  export CHROMEOS_BUILD=6946
+  export CHROMEOS_BUILD=6947
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
@@ -45,7 +45,7 @@ else
 fi
 
 # Major version for Chrome.
-export CHROME_BRANCH=43
+export CHROME_BRANCH=44
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
 export CHROME_BASE=
