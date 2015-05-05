5fdd6087edc5da79c5ba56f1c8bed4689c7ad9c9
Dharani Govindan <dharani@google.com>
2015-05-05 15:57:51 -0700

    Bump build number after branching refs/heads/stabilize-7039.B
    Change-Id: I2e42d70730a1e4ffcf6baff0c5fff402530d49a5

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,7 +15,7 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  export CHROMEOS_BUILD=7039
+  export CHROMEOS_BUILD=7040
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
