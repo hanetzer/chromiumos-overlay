ecfa8809020da6b04b6e93e36ae0bdb7bce2cc24
Prathmesh Prabhu <pprabhu@chromium.org>
2015-08-10 17:40:18 -0700

    modemmanager: Explictly use gawk for version extraction.
    Some modemmanager related libraries use gawk extensions to awk to
    extract the version string. Explicitly use gawk in these cases because
    they break on systems that symlink awk to mawk.
    
    BUG=chromium:519089
    TEST=build on a builder that symlinks awk to mawk.
    
    Change-Id: I3b1bb1e5c197ae8d60cf4358572b82dc27b312d3
    Reviewed-on: https://chromium-review.googlesource.com/292501
    Trybot-Ready: Prathmesh Prabhu <pprabhu@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Tested-by: Prathmesh Prabhu <pprabhu@chromium.org>

--- net-libs/libmbim/files/chromeos-version.sh
+++ net-libs/libmbim/files/chromeos-version.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-exec awk '
+exec gawk '
 match($0, /m4_define\(\[mbim_(major|minor|micro)_version\], \[([0-9]+)\]\)/,
       matches) { version[matches[1]] = matches[2] }
 END { print version["major"] "." version["minor"] "." version["micro"] }' \
