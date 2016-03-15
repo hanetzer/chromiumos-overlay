823808b98b1b87aaa769925f7d72246a55f065a7
Gwendal Grignou <gwendal@chromium.org>
2016-03-15 00:34:03 -0700

    chromeos-accelerometer-init: Run only the necessary commands
    The current script tries sysfs entries for old and new accelerometer layout.
    By testing the presence of file "location", we only issue the right commands.
    Also, load sysfs trigger module if necessary. It is required for triggering
    from sysfs, but not by the sensors.
    
    BUG=chrome-os-partner:50851
    TEST=Check it works on Cyan and Samus.
    Check with sh -x and udev.conf debug logging that now error messages are
    issued.
    
    Change-Id: Ic8c4687a2da265da1d90f0ac5f034dc2b57b7102
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/332437

--- chromeos-base/chromeos-accelerometer-init/files/udev/accelerometer-init.sh
+++ chromeos-base/chromeos-accelerometer-init/files/udev/accelerometer-init.sh
@@ -7,50 +7,68 @@
 
 DEVICE=$1
 IIO_DEVICES=/sys/bus/iio/devices
-IIO_DEVICE_PATH=${IIO_DEVICES}/${DEVICE}
-LOCATION=$(cat "${IIO_DEVICE_PATH}/location")
+IIO_DEVICE_PATH="${IIO_DEVICES}/${DEVICE}"
+LOCATION_PATH="${IIO_DEVICE_PATH}/location"
+IIO_SINGLE_SENSOR_DIR=false
+test -f "${LOCATION_PATH}" && IIO_SINGLE_SENSOR_DIR=true
+
+if "${IIO_SINGLE_SENSOR_DIR}"; then
+  LOCATION=$(cat "${LOCATION_PATH}")
+fi
 
 # Sets pre-determined calibration values for the accelerometers.
 # The calibration values are fetched from the VPD.
 dump_vpd_log --full --stdout | \
   egrep "\"in_accel_[xyz]_(lid|base)_calib(bias|scale)\"=" | sed 's/\"//g' | \
   while read key_value; do
-    CALIBRATION_NAME=$(echo ${key_value} | cut -d= -f1)
-    CALIBRATION_VALUE=$(echo ${key_value} | cut -d= -f2)
-    echo ${CALIBRATION_VALUE} > ${IIO_DEVICE_PATH}/${CALIBRATION_NAME}
-    # After kernel 3.18, it has separated iio:device for each acceleroemter.
-    # It needs to read values from in_accel_[xyz]_(lid|base)_calibbias in
-    # VPD and writes it into in_accel_[xyz]_calibbias in sysfs.
-    # _caliscale is not required now.
-    if (echo ${CALIBRATION_NAME} | grep -q "${LOCATION}_calibbias"); then
-      NEW_CALIBRATION_NAME=$(echo ${CALIBRATION_NAME} |
-                             sed 's/'"${LOCATION}"'_//;')
-      echo ${CALIBRATION_VALUE} > ${IIO_DEVICE_PATH}/${NEW_CALIBRATION_NAME}
+    IFS='=' read CALIBRATION_NAME CALIBRATION_VALUE <<EOL
+${key_value}
+EOL
+
+    if "${IIO_SINGLE_SENSOR_DIR}"; then
+      # After kernel 3.18, it has separated iio:device for each accelerometer.
+      # It needs to read values from in_accel_[xyz]_(lid|base)_calibbias in
+      # VPD and writes it into in_accel_[xyz]_calibbias in sysfs.
+      # _caliscale is not required now.
+      case "${CALIBRATION_NAME}" in
+        *${LOCATION}_calibbias)
+          NEW_CALIBRATION_NAME=$(echo "${CALIBRATION_NAME}" |
+                                 sed 's/${LOCATION}_//;')
+          echo "${CALIBRATION_VALUE}" > "${IIO_DEVICE_PATH}/${NEW_CALIBRATION_NAME}"
+          ;;
+      esac
+    else
+      echo "${CALIBRATION_VALUE}" > "${IIO_DEVICE_PATH}/${CALIBRATION_NAME}"
     fi
   done
 
-echo 0 > ${IIO_DEVICES}/iio_sysfs_trigger/add_trigger
-cat ${IIO_DEVICES}/iio_sysfs_trigger/trigger0/name > \
-    ${IIO_DEVICE_PATH}/trigger/current_trigger
+# Be sure the sysfs trigger module is present.
+modprobe -q iio_trig_sysfs
+echo 0 > "${IIO_DEVICES}/iio_sysfs_trigger/add_trigger"
+cat "${IIO_DEVICES}/iio_sysfs_trigger/trigger0/name" > \
+    "${IIO_DEVICE_PATH}/trigger/current_trigger"
 
-# Set up fields to probe on trigger.
-# Original fields for backwards compatibility with older kernel
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_x_base_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_y_base_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_z_base_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_x_lid_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_y_lid_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_z_lid_en
-# Fields for current kernel
-echo 0 > ${IIO_DEVICE_PATH}/scan_elements/in_timestamp_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_x_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_y_en
-echo 1 > ${IIO_DEVICE_PATH}/scan_elements/in_accel_z_en
+echo 0 > "${IIO_DEVICE_PATH}/scan_elements/in_timestamp_en"
+
+if "${IIO_SINGLE_SENSOR_DIR}"; then
+  # Fields for current kernel
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_x_en"
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_y_en"
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_z_en"
+else
+  # Set up fields to probe on trigger.
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_x_base_en"
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_y_base_en"
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_z_base_en"
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_x_lid_en"
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_y_lid_en"
+  echo 1 > "${IIO_DEVICE_PATH}/scan_elements/in_accel_z_lid_en"
+fi
 
 # We only fetch 1 sample at a time as Chrome is the only consumer.
-echo 1 > ${IIO_DEVICE_PATH}/buffer/length
-echo 1 > ${IIO_DEVICE_PATH}/buffer/enable
+echo 1 > "${IIO_DEVICE_PATH}/buffer/length"
+echo 1 > "${IIO_DEVICE_PATH}/buffer/enable"
 
 # Allow chronos to trigger the accelerometer.
-chgrp chronos ${IIO_DEVICES}/trigger0/trigger_now
-chmod g+w ${IIO_DEVICES}/trigger0/trigger_now
+chgrp chronos "${IIO_DEVICES}/trigger0/trigger_now"
+chmod g+w "${IIO_DEVICES}/trigger0/trigger_now"
