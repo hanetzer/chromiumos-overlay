0ec4e76b7b4f540428475ed6d245379799286a59
Mike Frysinger <vapier@chromium.org>
2016-10-27 16:26:13 -0400

    chromeos_version.sh: do not export version vars
    We don't need to export these to the env because the only consumers
    are build scripts which source this directly.
    
    BUG=chromium:659874
    TEST=precq still passes and has right version info in image
    
    Change-Id: I849655f1e4d3a546d339c122914d2dfdaece6ee8
    Reviewed-on: https://chromium-review.googlesource.com/406708
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- chromeos/config/chromeos_version.sh
+++ chromeos/config/chromeos_version.sh
@@ -15,43 +15,60 @@
 if [ -z "${FLAGS_version}" ]; then
   # Release Build number.
   # Increment by 1 for every release build.
-  export CHROMEOS_BUILD=8972
+  CHROMEOS_BUILD=8972
 
   # Release Branch number.
   # Increment by 1 for every release build on a branch.
   # Reset to 0 when increasing release build number.
-  export CHROMEOS_BRANCH=0
+  CHROMEOS_BRANCH=0
 
   # Patch number.
   # Increment by 1 in case a non-scheduled branch release build is necessary.
   # Reset to 0 when increasing branch number.
-  export CHROMEOS_PATCH=0
+  CHROMEOS_PATCH=0
 
   # Official builds must set CHROMEOS_OFFICIAL=1.
   if [ ${CHROMEOS_OFFICIAL:-0} -ne 1 ]; then
     # For developer builds, overwrite CHROMEOS_PATCH with a date string
     # for use by auto-updater.
-    export CHROMEOS_PATCH=$(date +%Y_%m_%d_%H%M)
+    CHROMEOS_PATCH=$(date +%Y_%m_%d_%H%M)
   fi
 
   # Version string. Not indentied to appease bash.
-  export CHROMEOS_VERSION_STRING=\
+  CHROMEOS_VERSION_STRING=\
 "${CHROMEOS_BUILD}.${CHROMEOS_BRANCH}.${CHROMEOS_PATCH}"
 else
-  export CHROMEOS_BUILD=$(echo ${FLAGS_version} | cut -f 1 -d ".")
-  export CHROMEOS_BRANCH=$(echo ${FLAGS_version} | cut -f 2 -d ".")
-  export CHROMEOS_PATCH=$(echo ${FLAGS_version} | cut -f 3 -d ".")
-  export CHROMEOS_VERSION_STRING="${FLAGS_version}"
+  CHROMEOS_BUILD=$(echo "${FLAGS_version}" | cut -f 1 -d ".")
+  CHROMEOS_BRANCH=$(echo "${FLAGS_version}" | cut -f 2 -d ".")
+  CHROMEOS_PATCH=$(echo "${FLAGS_version}" | cut -f 3 -d ".")
+  CHROMEOS_VERSION_STRING="${FLAGS_version}"
 fi
 
 # Major version for Chrome.
-export CHROME_BRANCH=56
+CHROME_BRANCH=56
 # Set CHROME values (Used for releases) to pass to chromeos-chrome-bin ebuild
 # URL to chrome archive
-export CHROME_BASE=
-# export CHROME_VERSION from incoming value or NULL and let ebuild default
-export CHROME_VERSION="$CHROME_VERSION"
+CHROME_BASE=
+# Set CHROME_VERSION from incoming value or NULL and let ebuild default.
+: "${CHROME_VERSION:=}"
 
-# Print (and remember) version info.
-echo "ChromeOS version information:"
-env | egrep '^CHROMEOS_VERSION|CHROME_' | sed 's/^/    /'
+# Print (and remember) version info.  We do each one by hand because there might
+# be more/other vars in the env already that start with CHROME_ or CHROMEOS_.
+echo "Chromium OS version information:"
+(
+# Subshell to hide the show_vars definition.
+show_vars() {
+  local v
+  for v in "$@"; do
+    eval echo \""    ${v}=\${${v}}"\"
+  done
+}
+show_vars \
+  CHROME_BASE \
+  CHROME_BRANCH \
+  CHROME_VERSION \
+  CHROMEOS_BUILD \
+  CHROMEOS_BRANCH \
+  CHROMEOS_PATCH \
+  CHROMEOS_VERSION_STRING
+)
