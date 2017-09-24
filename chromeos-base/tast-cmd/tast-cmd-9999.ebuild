hanetzer/chromiumos-overlay

commit da14389d80d53391b8dba81c52e7938e08a75d7d

tast-cmd: Build chromiumos/tast/cmd.
Update the tast-cmd ebuild to build chromiumos/tast/cmd,
formerly named chromiumos/tast/tast.

BUG=chromium:737628
TEST=built with FEATURES=test
CQ-DEPEND=I850392d0c35f93f547a90c2fc9a6d8a88be65c36

Change-Id: I549c5e053efba3bab7f0e1822c1c285665901b14
Reviewed-on: https://chromium-review.googlesource.com/676275
Commit-Ready: Dan Erat <derat@chromium.org>
Tested-by: Dan Erat <derat@chromium.org>
Reviewed-by: Jason Clinton <jclinton@chromium.org>

chromeos-base/tast-cmd/tast-cmd-9999.ebuild
@@ -6,11 +6,11 @@ CROS_WORKON_PROJECT="chromiumos/platform/tast"
 CROS_WORKON_LOCALNAME="tast"
 
 CROS_GO_BINARIES=(
-	"chromiumos/tast/tast"
+	"chromiumos/tast/cmd"
 )
 
 CROS_GO_TEST=(
-	"chromiumos/tast/tast/..."
+	"chromiumos/tast/cmd/..."
 )
 
 inherit cros-go cros-workon

commit 30545fe3982bfbbcfb81f0d0e7add620681b7a19

tast-cmd: Assign executable name.
Fix a bug introduced by da14389d80d5, which handled a
directory being renamed from "tast" to "cmd" but failed to
assign a hardcoded "tast" name for the resulting executable.

BUG=chromium:737628
TEST=verified that tast-cmd installs /usr/bin/tast instead
     of /usr/bin/cmd
TBR=jclinton@chromium.org

Change-Id: I72b1feb974173452fea029a2d5734b885c4f73da
Reviewed-on: https://chromium-review.googlesource.com/680534
Commit-Ready: Dan Erat <derat@chromium.org>
Tested-by: Dan Erat <derat@chromium.org>
Reviewed-by: Jason Clinton <jclinton@chromium.org>
Reviewed-by: Dan Erat <derat@chromium.org>

chromeos-base/tast-cmd/tast-cmd-9999.ebuild
@@ -6,7 +6,7 @@ CROS_WORKON_PROJECT="chromiumos/platform/tast"
 CROS_WORKON_LOCALNAME="tast"
 
 CROS_GO_BINARIES=(
-	"chromiumos/tast/cmd"
+	"chromiumos/tast/cmd:tast"
 )
 
 CROS_GO_TEST=(