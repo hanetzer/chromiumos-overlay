c0e33acc304a70fb760ce1e0f6554fcee0ee35a7
Caroline Tice <cmtice@google.com>
2018-03-28 15:42:23 -0700

    [clang-tidy] Update log parsing script.
    This adds flags allowing the user the specify the input and output
    files/directories; tries to add the board name to the output directories,
    and the package name and date to the output file names (making it
    possible to run clang-tidy on multiple packages, not just Chrome).
    
    BUG=chromium:826591
    TEST=Ran script with various new input/output designations and
    they all seemed to work.
    
    Change-Id: I773ba76ed5928adc891968e337348f5cefc482d1
    Reviewed-on: https://chromium-review.googlesource.com/985313
    Commit-Ready: Caroline Tice <cmtice@chromium.org>
    Tested-by: Caroline Tice <cmtice@chromium.org>
    Reviewed-by: Caroline Tice <cmtice@chromium.org>

--- sys-devel/llvm/files/clang-tidy-parse-build-log.py
+++ sys-devel/llvm/files/clang-tidy-parse-build-log.py
@@ -7,32 +7,85 @@
 
 from __future__ import print_function
 
+import argparse
+import datetime
 import os
 import sys
 
 import clang_tidy_execute
 
-def usage_error(err_msg):
-  print(err_msg)
-  print('Usage:  clang-tidy-parse-build-log.py <build_log>')
-  sys.exit(1)
+def Main(argv):
+  parser = argparse.ArgumentParser()
 
-def Main(args):
-  if len(args) != 1:
-    usage_error('Wrong number of args!')
+  parser.add_argument(
+      '--output_dir',
+      dest='out_dir',
+      default='',
+      help='Directory in which to write the resulting html & csv file.')
+  parser.add_argument(
+      '--log_file',
+      dest='log_file',
+      default=None,
+      required=True,
+      help='File containing the clang-tidy warnings to be parsed.')
+
+  options = parser.parse_args(argv)
+
+  output_dir = options.out_dir if options.out_dir else '/tmp/clang-tidy-output'
 
   cwd = os.path.dirname(os.path.realpath(__file__))
   warn_script = os.path.join(cwd, 'clang-tidy-warn.py')
 
-  logfile = args[0]
+  logfile = options.log_file
   if not os.path.exists(logfile):
-    usage_error('Cannot find log file "%s"' % logfile)
+    parser.error('Cannot find log file "%s"' % logfile)
 
   if not os.path.exists(warn_script):
-    usage_error('Cannot find %s' % warn_script)
+    parser.error('Cannot find %s' % warn_script)
 
-  warnfile = 'warnings.html'
-  warnfile_csv = 'warnings.csv'
+  # Normally, ChromeOS build logs have a filename format like:
+  # 'pkg-part1:pkg-part2:date-time.log'.  Below we parse this to find the
+  # package name(s).  We use these to create the warnings file names:
+  # 'date.package.warnings.html' and 'date.package.warnings.csv'.
+  # If filename does not conform to ChromeOS build log format, use full
+  # filename rather than the package name.
+
+  dirname, filename = os.path.split(logfile)
+  filename_bits = filename.split(':')
+  timestamp = ''
+  if len(filename_bits) >= 3:
+    package = '%s-%s' % (filename_bits[0], filename_bits[1])
+    time_parts = filename_bits[2].split('-')
+    timestamp = time_parts[0]
+  else:
+    package = filename
+
+  if not timestamp:
+    # Get a string with the current date, in the format 'YYYYMMDD'.
+    timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
+
+  html_filename = '%s.%s.warnings.html' % (timestamp, package)
+  csv_filename = '%s.%s.warnings.csv' % (timestamp, package)
+
+  # If the user did not specify a particular output directory and the logs
+  # appear to be in the default input directory, which contains the board name,
+  # extract the board name from the input directory and add it to the default
+  # output directory name.
+  if not options.out_dir:
+    dirname_bits = dirname.split('/')
+    if dirname[0] == '/' and dirname_bits[0] == '':
+      dirname_bits = dirname_bits[1:]
+    if (len(dirname_bits) == 3 and dirname_bits[0] == 'tmp' and
+        dirname_bits[1] == 'clang-tidy-logs'):
+      board = dirname_bits[2]
+      output_dir = os.path.join (output_dir, board)
+
+  # Create the output directory if it does not already exist.
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+  warnfile = os.path.join(output_dir, html_filename)
+  warnfile_csv = os.path.join(output_dir, csv_filename)
 
   result = clang_tidy_execute.Execute('python %s %s ' % (
       warn_script, logfile) +
