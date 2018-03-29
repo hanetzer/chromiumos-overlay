5715fbe558204c45287950663ecc134d2f583e2f
Amin Hassani <ahassani@chromium.org>
2018-03-29 16:09:10 -0700

    bsdiff: fix chromeos-version.sh
    fix chromeos-version.sh to read from the proper file from bsdiff source (not
    from platform2).
    
    BUG=none
    TEST=none
    
    Change-Id: If087b8030416dc4d8a7546136ef40ea1adc9d3db
    Reviewed-on: https://chromium-review.googlesource.com/987097
    Commit-Ready: Amin Hassani <ahassani@chromium.org>
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>

--- dev-util/bsdiff/files/chromeos-version.sh
+++ dev-util/bsdiff/files/chromeos-version.sh
@@ -2,4 +2,4 @@
 # Copyright 2015 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-exec awk '$1 == "Version:" {print $2}' "$1"/README.chromium
+exec awk '$1 == "Version:" {print $2}' "$2"/README.chromium
